e5bcbf5230111067148b1ba2f9e4a791968dbe44
the8127project <christoph.f.hug@gmail.com>
2022-01-26 20:26:05 +0100

    finished 26. Jan

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from os import listdir
 from os.path import isfile, join
 
 def create_copy_command():
-    path = '../LevelEditor/pngs/characters/evil'
+    path = '../LevelEditor/pngs/characters/good'
     onlydir = [f for f in sorted(listdir(path)) if not isfile(join(path, f))]
     for i in onlydir:
 
@@ -17,6 +17,11 @@ def create_copy_command():
             if (x.lower().find('stone') >= 0): position = 'stone'
             if (x.lower().find('anger') >= 0): position = 'anger'
             if (x.lower().find('flight') >= 0): position = 'flight'
+            if (x.lower().find('jump') >= 0): position = 'jump'
+            if (x.lower().find('high_jump') >= 0): position = 'high_jump'
+            if (x.lower().find('run') >= 0): position = 'run'
+            if (x.lower().find('run+attack') >= 0): position = 'run+attack'
+            if (x.lower().find('walk+attack') >= 0): position = 'walk+attack'
             if (x.lower().find('blade') >= 0): position = 'blade'
             if (x.lower().find('fire') >= 0): position = 'fire'
             if (x.lower().find('lightning') >= 0): position = 'lightning'
@@ -28,7 +33,9 @@ def create_copy_command():
             if (x.lower().find('explosion') >= 0): position = 'explosion'
             if (x.lower().find('web') >= 0): position = 'web'
             #x = x.lower().replace('idle', 'xidle')
+
             if (position != 'none'):
+
                 # print(position, x.lower(), x.lower().find('walk') )
                 if (position == 'xidle' and x[-5:-4] == '1'):
                     print('cp ', join(path2, x), path + '/' + i.lower() + '_'+position + '_' + x.lower()[:-5] + '_series999' + '.png')
@@ -63,6 +70,11 @@ def process_filelist(pathtofiles, prefix = 't', solid = 10, hurtlevel = 0, enemy
                 if (x.lower().find('stone') >= 0): position = 'stone'
                 if (x.lower().find('anger') >= 0): position = 'anger'
                 if (x.lower().find('flight') >= 0): position = 'flight'
+                if (x.lower().find('jump') >= 0): position = 'jump'
+                if (x.lower().find('high_jump') >= 0): position = 'high_jump'
+                if (x.lower().find('run') >= 0): position = 'run'
+                if (x.lower().find('run+attack') >= 0): position = 'run+attack'
+                if (x.lower().find('walk+attack') >= 0): position = 'walk+attack'
                 if (x.lower().find('blade') >= 0): position = 'blade'
                 if (x.lower().find('fire') >= 0): position = 'fire'
                 if (x.lower().find('lightning') >= 0): position = 'lightning'
@@ -70,7 +82,7 @@ def process_filelist(pathtofiles, prefix = 't', solid = 10, hurtlevel = 0, enemy
                 if (x.lower().find('walk') >= 0): position = 'walk'
                 if (x.lower().find('death') >= 0): position = 'death'
                 if (x.lower().find('hurt') >= 0): position = 'hurt'
-                if (x.lower().find('idle') >= 0): position = 'idle'
+                if (x.lower().find('idle') >= 0): position = 'xidle'
                 if (x.lower().find('explosion') >= 0): position = 'explosion'
                 if (x.lower().find('web') >= 0): position = 'web'
 
@@ -108,16 +120,20 @@ objectlist.append({"Level":"objects", "Content": object})
 
 #enemies
 path = '../LevelEditor/pngs/characters/evil/'
-#object = process_filelist(path, prefix = 'e', solid = 0, hurtlevel = 5, enemy=1, collect = 0, food = 0, heart= 0)
-#objectlist.append({"Level":"evil", "Content": object})
+object = process_filelist(path, prefix = 'e', solid = 0, hurtlevel = 5, enemy=1, collect = 0, food = 0, heart= 0)
+objectlist.append({"Level":"evil", "Content": object})
 
+#good
+path = '../LevelEditor/pngs/characters/good/'
+object = process_filelist(path, prefix = 'e', solid = 0, hurtlevel = 5, enemy=0, collect = 0, food = 0, heart= 0)
+objectlist.append({"Level":"good", "Content": object})
 
 #path = '../LevelEditor/pngs/worlds/castle/enemies/'
 #object = process_filelist(path)
 #objectlist.append({"Level":"enemies", "Content": object})
 
 #food
-create_copy_command()
+#create_copy_command()
 #potion
 
 with open('../LevelEditor/pngs/level1.json', 'w',  encoding='utf-8') as JSONfile:
